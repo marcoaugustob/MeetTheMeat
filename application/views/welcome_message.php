8e2a08c8d103131f21322f3f536836d00a3a6799
marcoaugustob <marcoaugustob@gmail.com>
2018-04-22 22:20:30 -0300

    create, list e ajuste de modais

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,40 +12,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
       <div class="container">
-        <h1>Bem vindo ao Meet The Meat!</h1>
+      <body> 
+      <h1>Bem vindo ao Meet The Meat!</h1>
         <p>Projeto acedemico que avalia a qualidade das carnes e fornecedores.</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+          <button class="btn btn-primary btn-lg">Novo Review <? echo anchor('review/reviewCreate', 'Novo Review') ?></li>
+          <li>Lista de Reviews<? echo anchor('review/listReviews', 'Lista de Reviews') ?></li>
+        </ul>
+</body>
       </div>
     </div>
-
     <div class="container">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p> </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-      </div>
 
+     
       <hr>
 
       <footer>
-        <p>&copy; 2016 Company, Inc.</p>
+        <p style="text-align: center;">&copy; <?php
+                                              echo date("Y");
+                                              ?> Marco e Lucas, Inc.</p>
       </footer>
-    </div> <!-- /container -->
-
+    </div> 
 </div>
 
 </body>
 </html>
+
